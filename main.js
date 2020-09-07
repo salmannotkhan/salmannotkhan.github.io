37b4edd14cb6ea3432fb1668e2da09a263d57d21
Salman Shaikh <tony903212@gmail.com>
2020-09-07 22:44:32 +0530

    Added Projects, Added Fiverr link in contact

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ btn = document.querySelector('nav #button')
 active = document.querySelector('.active')
 links = document.querySelectorAll('nav a')
 hours = new Date().getHours()
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
 function nightMode(){
     document.querySelector('body').classList.toggle('dark')
     btn.children[0].classList.toggle('fa-sun')
@@ -17,6 +19,22 @@ links.forEach(link => {
         document.querySelector(this.dataset.target).scrollIntoView()
     })
 })
+
+function scrollSlider() {
+    logoSlider.scroll(xscroll, 0)
+    if(Math.floor(logoSlider.scrollLeft / 250) == 2){
+        xscroll = 10
+    }
+    else {
+        xscroll = xscroll + logoSlider.scrollLeft + 200
+    }
+    scrolldelay = setTimeout(scrollSlider,2000);
+}
+if (width < 768){
+    logoSlider = document.querySelector('.lang-logos')
+    xscroll = 10
+    scrollSlider()
+}
 
 btn.addEventListener('click', nightMode)
 if (hours > 18 || hours < 6){
