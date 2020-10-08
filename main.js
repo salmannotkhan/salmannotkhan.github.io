758bc45013288616ea1a44dade769aee42577e57
Salman Shaikh <tony903212@gmail.com>
2020-10-08 23:51:58 +0530

    Saving user preference for Dark Mode Removed auto Dark Mode after 6 PM

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-btn = document.querySelector("nav #button");
-active = document.querySelector(".active");
-links = document.querySelectorAll("nav a");
-hours = new Date().getHours();
+btn = document.querySelector("nav #button")
+active = document.querySelector(".active")
+links = document.querySelectorAll("nav a")
 function nightMode() {
-    document.querySelector("body").classList.toggle("dark");
+    document.querySelector("body").classList.toggle("dark")
     if (document.querySelector("body").classList.contains("dark")) {
-        btn.children[0].src = "images/sun-icon.webp";
+        btn.children[0].src = "images/sun-icon.webp"
+        return true
     } else {
-        btn.children[0].src = "images/moon-icon.webp";
+        btn.children[0].src = "images/moon-icon.webp"
+        return false
     }
 }
 links.forEach((link) => {
     link.addEventListener("click", function () {
         links.forEach((link) => {
-            link.classList.remove("active");
-        });
-        active.style.top = this.offsetTop + "px";
-        document.querySelector(this.dataset.target).scrollIntoView();
-    });
-});
-btn.addEventListener("click", nightMode);
-if (hours > 18 || hours < 6) {
-    nightMode();
+            link.classList.remove("active")
+        })
+        active.style.top = this.offsetTop + "px"
+        document.querySelector(this.dataset.target).scrollIntoView()
+    })
+})
+btn.addEventListener("click", ()=>{
+    localStorage.setItem('darkmode', nightMode())
+})
+if (localStorage.darkmode == 'true'){
+    nightMode()
 }
-document.querySelector("#home").scrollIntoView();
+document.querySelector("#home").scrollIntoView()
