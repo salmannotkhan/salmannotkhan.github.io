93b9ee22649158a4aba4a25b3e4620e5c905ef9e
Salman Shaikh <tony903212@gmail.com>
2021-07-04 00:00:56 +0530

    Added Project cards

---
+++ src/Project.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Project.css';
+
+class Project extends React.Component {
+	state = {
+		clicked: '',
+	};
+
+	handleRipple = (e, btn) => {
+		this.setState({ clicked: btn }, () => {
+			e.target.previousSibling.style.top = e.nativeEvent.layerY + 'px';
+			e.target.previousSibling.style.left = e.nativeEvent.layerX + 'px';
+		});
+		setTimeout(() => {
+			this.setState({ clicked: '' });
+		}, 1500);
+	};
+	render() {
+		return (
+			<div className="project">
+				<div className="details">
+					<div className="title">{this.props.title}</div>
+					<div className="description">{this.props.desc}</div>
+					<div className="lang">Written in: {this.props.lang}</div>
+				</div>
+				<div className="links">
+					<div className="link" onClick={(e) => this.handleRipple(e, 'github')}>
+						{this.state.clicked === 'github' ? (
+							<div className="ripple"> </div>
+						) : null}
+						<a href={this.props.githubUrl}>Github Link</a>
+					</div>
+					{this.props.url ? (
+						<div className="link" onClick={(e) => this.handleRipple(e, 'url')}>
+							{this.state.clicked === 'url' ? (
+								<div className="ripple"> </div>
+							) : null}
+							<a href={this.props.url}>Visit Site</a>
+						</div>
+					) : null}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Project;
--- src/ProjectsScreen.js
+++ src/ProjectsScreen.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Project from './Project';
 import './ProjectsScreen.css';
 
 class ProjectsScreen extends React.Component {
+	state = {
+		projects: [],
+	};
+
+	fixCase = (x) => {
+		x = x.replace(/[_-]/, ' ');
+		if (x.indexOf(' ') === -1) {
+			return x;
+		}
+		x = x.toLowerCase().split(' ');
+		for (let i = 0; i < x.length; i++) {
+			x[i] = x[i][0].toUpperCase() + x[i].slice(1);
+		}
+		return x.join(' ');
+	};
+	async componentDidMount() {
+		const url = 'https://api.github.com/users/salmannotkhan/starred';
+		const response = await fetch(url);
+		const projects = await response.json();
+		this.setState({ projects });
+	}
 	render() {
 		return (
 			<div id="projects">
@@ -12,7 +34,20 @@ class ProjectsScreen extends React.Component {
 						<img src="images/php-logo.webp" alt="php" />
 					</div>
 				</div>
-				<div className="project-list">&nbsp;</div>
+				<div className="project-list">
+					{this.state.projects.map((project) =>
+						project.owner.login === 'salmannotkhan' ? (
+							<Project
+								key={project.id}
+								title={this.fixCase(project.name)}
+								desc={project.description}
+								lang={project.language}
+								githubUrl={project.html_url}
+								url={project.homepage}
+							/>
+						) : null
+					)}
+				</div>
 			</div>
 		);
 	}
