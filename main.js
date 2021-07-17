cb0b104957a3232a0a2bca0fdba17584ee5fdfd0
Salman Shaikh <tony903212@gmail.com>
2021-07-17 13:04:01 +0530

    Changed navigation technique to anchor's href made nightMode() bit more tidy

--- main.js
+++ main.js
@@ -4,22 +4,16 @@ links = document.querySelectorAll("nav a")
 hours = new Date().getHours()
 var timeout
 function nightMode() {
-    document.querySelector("body").classList.toggle("dark")
-    if (document.querySelector("body").classList.contains("dark")) {
-        btn.children[0].src = "images/sun-icon.webp"
-        return true
-    } else {
-        btn.children[0].src = "images/moon-icon.webp"
-        return false
-    }
+    const darkMode = document.querySelector("body").classList.toggle("dark")
+    btn.children[0].src = `images/${darkMode ? "sun": "moon"}-icon.webp`
+    return darkMode
+}
+if (location.hash !== '') {
+    active.style.top = document.querySelector(`a[href="${location.hash}"]`).offsetTop + 'px'
 }
 links.forEach((link) => {
     link.addEventListener("click", function () {
-        links.forEach((link) => {
-            link.classList.remove("active")
-        })
         active.style.top = this.offsetTop + "px"
-        document.querySelector(this.dataset.target).scrollIntoView()
     })
 })
 btn.addEventListener("click", ()=>{
@@ -46,4 +40,3 @@ else{
         }, 5000);
     }, 5000);
 }
-document.querySelector("#home").scrollIntoView()
