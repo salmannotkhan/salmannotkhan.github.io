457fb4e7f9768afd1ab7de45789097e7a268abc3
Salman Shaikh <tony903212@gmail.com>
2021-12-12 18:37:21 +0530

    Added Upwork link in contact
    switched to spaces for indentation

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-btn = document.querySelector("nav #button")
-active = document.querySelector(".active")
-links = document.querySelectorAll("nav a")
-hours = new Date().getHours()
-var timeout
+btn = document.querySelector("nav #button");
+active = document.querySelector(".active");
+links = document.querySelectorAll("nav a");
+hours = new Date().getHours();
+var timeout;
 function nightMode() {
-    const darkMode = document.querySelector("body").classList.toggle("dark")
-    btn.children[0].src = `images/${darkMode ? "sun": "moon"}-icon.webp`
-    return darkMode
+    const darkMode = document.querySelector("body").classList.toggle("dark");
+    btn.children[0].src = `images/${darkMode ? "sun" : "moon"}-icon.webp`;
+    return darkMode;
 }
-if (location.hash !== '') {
-    active.style.top = document.querySelector(`a[href="${location.hash}"]`).offsetTop + 'px'
-    setTimeout(()=>document.querySelector(location.hash).scrollIntoView(), 100)
+if (location.hash !== "") {
+    active.style.top =
+        document.querySelector(`a[href="${location.hash}"]`).offsetTop + "px";
+    setTimeout(
+        () => document.querySelector(location.hash).scrollIntoView(),
+        100
+    );
 }
 links.forEach((link) => {
     link.addEventListener("click", function () {
-        active.style.top = this.offsetTop + "px"
-    })
-})
-btn.addEventListener("click", ()=>{
-    clearTimeout(timeout)
-    localStorage.setItem('darkmode', nightMode())
-})
-if (localStorage.darkmode == 'true'){
-    nightMode()
-}
-else{
-    if (hours > 18 || hours < 6) {
-        btn.children[1].innerHTML = 'Save your eyes'
+        active.style.top = this.offsetTop + "px";
+    });
+});
+btn.addEventListener("click", () => {
+    clearTimeout(timeout);
+    localStorage.setItem("darkmode", nightMode());
+});
+if (localStorage.darkmode == "true") {
+    nightMode();
+} else {
+    console.log(hours);
+    if (hours >= 18 || hours <= 6) {
+        btn.children[1].innerHTML = "Save your eyes";
     }
     timeout = setTimeout(() => {
-        btn.children[1].style.marginLeft = '10px'
+        btn.children[1].style.marginLeft = "10px";
         setTimeout(() => {
-            btn.children[1].style.opacity = 1
+            btn.children[1].style.opacity = 1;
         }, 500);
         setTimeout(() => {
-            btn.children[1].style.opacity = 0
+            btn.children[1].style.opacity = 0;
             setTimeout(() => {
-                btn.children[1].style = ''
+                btn.children[1].style = "";
             }, 500);
         }, 5000);
     }, 5000);
